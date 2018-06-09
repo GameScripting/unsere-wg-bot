0a3ba11a3aa6611cd31214c6fc3f18d2efb4fe62
Daniel Huhn <daniel@danielhuhn.de>
2018-06-09 16:10:17 +0200

    fixed typo on deployment script

--- buildAndDeployToPi.sh
+++ buildAndDeployToPi.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -e
+
 HOST=pi@raspberrypi
 
 ./gradlew bootJar
 
 scp build/libs/unsere-wg-bot-0.1.jar $HOST:/home/pi/unsere-wg-bot
-ssh $HOST "sudo systemctrl restart unserewgbot"
+ssh $HOST "sudo systemctl restart unserewgbot"
+
+echo "SUCCESS!"
