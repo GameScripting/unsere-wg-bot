7829a4da2e8ccabe1b86faf960a9319983164de6
Daniel Huhn <daniel@danielhuhn.de>
2018-11-10 15:15:33 +0100

    re-implemented the bot to work properly with groups

--- src/main/kotlin/org/github/gamescripting/unsereWgBot/UnsereWgBot.kt
+++ src/main/kotlin/org/github/gamescripting/unsereWgBot/UnsereWgBot.kt
@@ -1,127 +1,158 @@
 package org.github.gamescripting.unsereWgBot
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.github.gamescripting.unsereWgBot.models.Person
 import org.github.gamescripting.unsereWgBot.network.Fritzbox
 import org.github.gamescripting.unsereWgBot.reposiories.PeopleRepository
 import org.springframework.stereotype.Component
 import org.telegram.abilitybots.api.bot.AbilityBot
-import org.telegram.abilitybots.api.objects.Flag
+import org.telegram.abilitybots.api.objects.MessageContext
 import org.telegram.telegrambots.api.methods.send.SendMessage
 import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup
 import org.telegram.telegrambots.api.objects.replykeyboard.buttons.InlineKeyboardButton
 import java.util.function.Consumer
-import java.util.function.Predicate
-
-private val mapper = ObjectMapper()
 
 @Component
 class UnsereWgBot(
+        val telegramBotConfig: TelegramBotConfig,
         val fritzbox: Fritzbox,
-        val peopleRepository: PeopleRepository,
-        val telegramBotConfig: TelegramBotConfig
+        val peopleRepository: PeopleRepository
 ) : AbilityBot(telegramBotConfig.botToken, telegramBotConfig.botUsername) {
+    override fun creatorId() = telegramBotConfig.creatorId
+            ?: throw Exception("Please set the telegramBot.creatorId in the config")
 
-    override fun creatorId(): Int {
-        return telegramBotConfig.creatorId ?: throw Exception("Please set the telegramBot.creatorId in the config")
+    companion object {
+        val MESSAGE_WERISTDA = "/weristda"
+        val MESSAGE_LEUTE = "/leute"
     }
 
-    var lastSelectedMacAdress: String? = null
+    var lastIncomingMessage: String? = null
+    var lastOutgoingMessageKey: MessageKey? = null
+    var lastCallbackQueryData: String? = null
+
+    fun default() = defaultAllPublicAbility("default", "Default handler").action(handleMessage()).build()
+    fun leute() = defaultAllPublicAbility("leute", "Zeigt alle Geräte an").action(handleMessage()).build()
+    fun weristda() = defaultAllPublicAbility("weristda", "Zeigt an wer da ist").action(handleMessage()).build()
+
+    private fun handleMessage(): Consumer<MessageContext> = Consumer { messageContext ->
+        val text = messageContext.update()?.message?.text
+        val callbackQueryData = messageContext.update()?.callbackQuery?.data
+
+        try {
+            ensureWgGroup(messageContext) {
+                when {
+                    text?.startsWith(MESSAGE_WERISTDA, ignoreCase = true) == true -> {
+                        showPeople(messageContext)
+                    }
+                    text?.startsWith(MESSAGE_LEUTE, ignoreCase = true) == true -> {
+                        showPeopleWithKeyboard(messageContext)
+                    }
+                    callbackQueryData != null && lastIncomingMessage.equals(MESSAGE_LEUTE, ignoreCase = true) -> {
+                        askForName(messageContext)
+                        lastOutgoingMessageKey = MessageKey.ASKED_FOR_NAME
+                    }
+                    lastOutgoingMessageKey == MessageKey.ASKED_FOR_NAME && text != null -> {
+                        addPerson(messageContext, text)
+                        lastCallbackQueryData = null
+                        lastOutgoingMessageKey = null
+                    }
+                    else -> showUnknownCommand(messageContext)
+                }
+            }
+        } finally {
+            if (text != null) {
+                lastIncomingMessage = text
+            }
+
+            if (callbackQueryData != null) {
+                lastCallbackQueryData = callbackQueryData
+            }
+        }
+    }
+
+    private fun addPerson(messageContext: MessageContext, text: String) {
+        val newName = text
+        val macAdress = lastCallbackQueryData ?: throw Exception("Invalid state, macAddress unknown")
+
+        val existingUser = peopleRepository.findByMacAdress(macAdress)
+        val updatedUser = if (existingUser != null) {
+            existingUser.displayName = newName
+            existingUser
+        } else {
+            Person().apply {
+                this.macAdress = macAdress
+                this.displayName = newName
+            }
+        }
+
+        peopleRepository.saveAndFlush(updatedUser)
+
+        silent.send("Alles klar, wir haben ${updatedUser.displayName} gespeichert.", messageContext.chatId())
+    }
+
+    private fun askForName(messageContext: MessageContext) {
+        silent.send("Wie lautet der Name?", messageContext.chatId())
+    }
+
+    private fun showPeopleWithKeyboard(messageContext: MessageContext) {
+        val networkDevices = fritzbox.readNetworkDevices()
+        val allPeople = peopleRepository.findAll()
+
+        val buttons = networkDevices.map { device ->
+            val person = allPeople.firstOrNull { it.macAdress == device.macAddress }
+
+            if (person == null) {
+                InlineKeyboardButton().apply {
+                    text = "${device.macAddress} (${device.hostName})"
+                    callbackData = device.macAddress
+                }
+            } else {
+                InlineKeyboardButton().apply {
+                    callbackData = device.macAddress
+                    text = person.displayName
+                }
+            }
+        }
+
+        val keyboard = buttons.windowed(2, 2, true)
+        val keyboardMarkup = InlineKeyboardMarkup().apply { this.keyboard = keyboard }
+
+        val sendMessage = SendMessage().apply {
+            chatId = messageContext.chatId().toString()
+            text = "Bitte wähle eine Person aus:"
+            replyMarkup = keyboardMarkup
+        }
+
+        silent.execute(sendMessage)
+    }
+
+    private fun showUnknownCommand(messageContext: MessageContext) {
+        silent.send("Unbekannter Befehl", messageContext.chatId())
+        lastOutgoingMessageKey = MessageKey.ASKED_FOR_NAME
+    }
+
+    private fun showPeople(messageContext: MessageContext) {
+        val macAdresses = fritzbox.getMacAdressesInNetwork()
+        val allPeople = peopleRepository.findAll()
+
+        val names = macAdresses.map { macAdress ->
+            allPeople.firstOrNull { it.macAdress == macAdress }
+        }.filterNotNull().map { it.displayName }
+
+        if (names.any()) {
+            val nameList = names.joinToString(separator = "\n") { "- $it" }
+            silent.send("Im moment sind da:\n$nameList", messageContext.chatId())
+        } else {
+            silent.send("Im moment ist niemand da.", messageContext.chatId())
+        }
+    }
 
     fun sayHelloAfterStart() {
-        for(groupId in telegramBotConfig.allowedGroupIds) {
+        for (groupId in telegramBotConfig.allowedGroupIds) {
             silent.send("Moin, ich bin dann wieder online 👋", groupId)
         }
     }
+}
 
-    fun werIstDax() = defaultAllPublicAbility("weristda", "Zeigt an, wer gerade da ist").action { messageContext ->
-        ensureWgGroup(messageContext) {
-            val macAdresses = fritzbox.getMacAdressesInNetwork()
-            val allPeople = peopleRepository.findAll()
-
-            val names = macAdresses.map { macAdress ->
-                allPeople.firstOrNull { it.macAdress == macAdress }
-            }.filterNotNull().map { it.displayName }
-
-            if (names.any()) {
-                val nameList = names.joinToString(separator = "\n") { "- $it" }
-                silent.send("Im moment sind da:\n$nameList", messageContext.chatId())
-            } else {
-                silent.send("Im moment ist niemand da.", messageContext.chatId())
-            }
-        }
-    }.build()
-
-    fun keyboardTest() = defaultAllPublicAbility("leute", "Erlaubt das hinzufügen eines neuen Namen")
-            .action {
-                val x = 5
-            }
-            .reply(Consumer { update ->
-                val networkDevices = fritzbox.readNetworkDevices()
-                val allPeople = peopleRepository.findAll()
-
-                val message = update.message ?: return@Consumer
-
-                ensureWgGroup(message) {
-                    val buttons = networkDevices.map { device ->
-                        val person = allPeople.firstOrNull { it.macAdress == device.macAddress }
-
-                        if (person == null) {
-                            InlineKeyboardButton().apply {
-                                text = "${device.macAddress} (${device.hostName})"
-                                callbackData = device.macAddress
-                            }
-                        } else {
-                            InlineKeyboardButton().apply {
-                                callbackData = device.macAddress
-                                text = person.displayName
-                            }
-                        }
-                    }
-
-                    val keyboard = buttons.windowed(2, 2, true)
-                    val keyboardMarkup = InlineKeyboardMarkup().apply { this.keyboard = keyboard }
-
-                    val sendMessage = SendMessage().apply {
-                        chatId = message.chatId.toString()
-                        text = "Bitte wähle eine Person aus:"
-                        replyMarkup = keyboardMarkup
-                    }
-
-                    silent.execute(sendMessage)
-                }
-            }, Predicate { it?.message?.text?.startsWith("/leute") == true })
-            .reply(Consumer { update ->
-                val callbackQuery = update.callbackQuery ?: return@Consumer
-                val message = callbackQuery.message ?: return@Consumer
-
-                ensureWgGroup(message) {
-                    lastSelectedMacAdress = callbackQuery.data
-                    silent.send("Wie lautet der Name?", message.chatId)
-                }
-            }, Flag.CALLBACK_QUERY)
-            .reply(Consumer { update ->
-                val message = update.message ?: return@Consumer
-                val macAdress = lastSelectedMacAdress ?: return@Consumer
-
-                ensureWgGroup(message) {
-                    val existingUser = peopleRepository.findByMacAdress(macAdress)
-                    val newName = message.text
-                    val updatedUser = if (existingUser != null) {
-                        existingUser.displayName = newName
-                        existingUser
-                    } else {
-                        Person().apply {
-                            this.macAdress = macAdress
-                            this.displayName = newName
-                        }
-                    }
-
-                    peopleRepository.saveAndFlush(updatedUser)
-
-                    silent.send("Alles klar, wir haben ${updatedUser.displayName} gespeichert.", message.chatId)
-                }
-            }, Predicate { it?.message?.text?.startsWith("/") == false })
-            .build()
-
+enum class MessageKey {
+    ASKED_FOR_NAME
 }
